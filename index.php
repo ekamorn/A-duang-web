cab04152967d635625ab54f2d1db5c81c7e6fce4
ekamorn <31618884+ekamorn@users.noreply.github.com>
2017-10-04 10:18:50 +0700

    add a duang index page

---
+++ index.php
@@ -0,0 +1,522 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Chrome theme -->
+	<meta name="theme-color" content="#2b3143">
+	<!-- Window phone theme -->
+	<meta name="msapplication-navbutton-color" content="#2b3143">
+	<!-- iOS safari -->
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+	<!-- Tab icon -->
+	<link rel="icon" type="image/png" href="image/tab_icon_2.png" sizes="32*32">
+
+	<title>a ดวง</title>
+
+	<!-- Bootstrap core css -->
+	<link rel="stylesheet" type="text/css" href="bootstrap-4.0.0-beta-dist/css/bootstrap.min.css">
+
+	<!-- owl carousel core css -->
+	<link rel="stylesheet" type="text/css" href="OwlCarousel2-2.2.1/dist/assets/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css" href="OwlCarousel2-2.2.1/dist/assets/owl.theme.default.min.css">
+
+	<!-- DB Airy font style -->
+	<link rel="stylesheet" type="text/css" href="fonts/stylesheet.css">
+
+	<!-- navbar css -->
+	<link rel="stylesheet" type="text/css" href="css/navbar.css">
+	<!-- custom css -->
+	<link rel="stylesheet" type="text/css" href="css/aduang-home-custom.css">
+	<!-- footer css -->
+	<link rel="stylesheet" type="text/css" href="css/footer.css">
+
+	<!-- Line and Facebook font icon -->
+	<link rel="stylesheet" type="text/css" href="icomoon/style.css">
+</head>
+<body>
+	<div id="all-wrap">
+		<!-- Preloader Wrapper -->
+		<div id="loader-wrapper">
+			<div id="loader"></div>
+
+			<div class="loader-section section-left"></div>
+			<div class="loader-section section-right"></div>
+		</div>
+
+		<section class="parallax-header">
+			<div class="wrapper">
+					
+				<div class="cover-backpic backpic">
+					<div class="head-label--collection text-collection">
+						<p class="head-label label">คิดคำตรงนี้ไม่ออก</p>	
+					</div>
+					
+					<div class="body-label--collection text-collection">
+						<p class="big-body-label label">a ดวง มิติใหม่</p>
+						<p class="small-body-label label">ของการดูดวง</p>
+					</div>
+				</div>	
+
+				<div id="mascot-1" class="mascot mascot-day1">
+					<img src="image/v02/d-1.png" class="img-responsive img-mascot">
+				</div>
+				<div id="mascot-2" class="mascot mascot-day2">
+					<img src="image/v02/d-2.png" class="img-responsive img-mascot">
+				</div>
+				<div id="mascot-3" class="mascot mascot-day3">
+					<img src="image/v02/d-3.png" class="img-responsive img-mascot">
+				</div>
+				<div id="mascot-4" class="mascot mascot-day4">
+					<img src="image/v02/d-4.png" class="img-responsive img-mascot">
+				</div>
+				<div id="mascot-5" class="mascot mascot-day5">
+					<img src="image/v02/d-5.png" class="img-responsive img-mascot">
+				</div>
+				<div id="mascot-6" class="mascot mascot-day6">
+					<img src="image/v02/d-6.png" class="img-responsive img-mascot">
+				</div>
+				<div id="mascot-7" class="mascot mascot-day7">
+					<img src="image/v02/d-7.png" class="img-responsive img-mascot">
+				</div>
+				<div id="mascot-8" class="mascot mascot-day8">
+					<img src="image/v02/d-8.png" class="img-responsive img-mascot">
+				</div>
+
+			</div>
+		</section>
+
+		<!-- Navigation -->
+		<?php
+		include 'navbar.php';
+		?>
+
+		<!-- Second content part -->
+		<div class="second-content--collection">
+			<div class="container-fluid">
+				<div class="row">
+
+					<img src="image/v02/Union-1.png" class="img-responsive img-pop pop-7 pop-trans-1">
+					<img src="image/v02/star-2.png" class="img-responsive img-pop pop-8 pop-trans-2">
+					<img src="image/v02/Ellipse-1.png" class="img-responsive img-pop pop-9 pop-trans-3">
+
+					<img src="image/v02/Ellipse-2.png" class="img-responsive img-pop pop-10 pop-trans-2">
+					<img src="image/v02/Union-2.png" class="img-responsive img-pop pop-11 pop-trans-1">
+					<img src="image/v02/star-2.png" class="img-responsive img-pop pop-12 pop-trans-2">
+					<img src="image/v02/star.png" class="img-responsive img-pop pop-13 pop-trans-3">
+
+					<div class="col-lg-8 col-md-8 col-sm-6 col-xl-8 col-12 weekly-horo-collection">
+						<!-- head label collection -->
+						<div class="weekly-head-collection">
+							<p class="weekly-big-head">ดวง<span>รายสัปดาห์</span></p>
+							<p class="weekly-small-head">ตามราศี</p>
+							
+							<img src="image/v02/thunder.png" class="img-responsive img-pop pop-1">
+							<img src="image/v02/star.png" class="img-responsive img-pop pop-2">
+							<img src="image/v02/circle.png" class="img-responsive img-pop pop-3">
+
+							<img src="image/v02/thunder.png" class="img-responsive img-pop pop-4">
+							<img src="image/v02/star.png" class="img-responsive img-pop pop-5">
+							<img src="image/v02/circle.png" class="img-responsive img-pop pop-6">
+						</div>
+
+						<!-- body collection -->
+						<div class="weekly-body-collection">
+							<div class="container">
+								<div class="row">
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/aries.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/taurus.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/gemini.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/cancer.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/leo.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/virgo.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/libra.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/scorpio.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/sajittarius.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/carpicorn.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/aquarius.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+									<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2 col-2 each-zodiac--collection">
+										<a class="zodiac-link" href="#">
+											<img src="image/v02/pisces.png" class="img-responsive img-zodiac-link">
+										</a>
+									</div>
+
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xl-4 col-4 daily-horo-tarot-collection">
+						<!-- head label collection -->
+						<div class="daily-head-collection">
+							<p class="daily-big-head">ดวง<span>รายวัน</span></p>
+							<p class="daily-small-head">กับไพ่ยิปซี</p>
+							
+							<img src="image/v02/thunder.png" class="img-responsive img-pop pop-1">
+							<img src="image/v02/star.png" class="img-responsive img-pop pop-2">
+							<img src="image/v02/circle.png" class="img-responsive img-pop pop-3">
+
+							<img src="image/v02/thunder.png" class="img-responsive img-pop pop-4">
+							<img src="image/v02/star.png" class="img-responsive img-pop pop-5">
+							<img src="image/v02/circle.png" class="img-responsive img-pop pop-6">
+						</div>
+
+						<!-- body card collection -->
+						<div class="daily-body-collection">
+							<div class="container">
+								<div class="row">
+									<div class="col-lg-12 col-md-12 col-sm-12 col-xl-12 col-12">
+										<div class="tarot-deck--collection" id="tarot-deck--collection">
+
+										<!-- deck tarot card here -->
+											
+										</div>
+
+										<div class="tarot-deck-button--collection">
+											<button class="tarot-card-submit-btn" onclick="">ดูคำทำนาย</button>
+										</div>
+
+										<!-- answer card here -->
+
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<!-- Third & Fourth content section -->
+		<div class="third-fourth-content-section">
+
+			<!-- Third Props -->
+			<img src="image/v02/Ellipse-4.png" class="img-responsive img-pop pop-14 pop-trans-3">
+			<img src="image/v02/Ellipse-3.png" class="img-responsive img-pop pop-15 pop-trans-2">
+			<img src="image/v02/Union-2.png" class="img-responsive img-pop pop-16 pop-trans-3">
+		
+			<img src="image/v02/star-2.png" class="img-responsive img-pop pop-18 pop-trans-2">
+
+			<!-- Fourth Props -->
+			<img src="image/v02/star.png" class="img-responsive img-pop pop-19 pop-trans-2">
+			<img src="image/v02/Union-1.png" class="img-responsive img-pop pop-20 pop-trans-3">
+			<img src="image/v02/Union-2.png" class="img-responsive img-pop pop-21 pop-trans-2">
+			<img src="image/v02/Ellipse-5.png" class="img-responsive img-pop pop-22 pop-trans-1">
+			<img src="image/v02/Ellipse-6.png" class="img-responsive img-pop pop-23 pop-trans-2">
+
+			<div class="container-fluid my-container">
+				
+				<!-- Third content section -->
+				<div class="third-content-section">
+					<div class="row">
+						<div class="col-lg-8 col-md-8 col-sm-8 col-8 col-xl-8 mx-auto article-horo-collection">
+							<!-- head label collection -->
+							<div class="article-head-collection">
+								<p class="article-big-head">บทความ<span>น่าอ่าน</span></p>
+								<p class="article-small-head">จาก a ดวง</p>
+								
+								<img src="image/v02/thunder.png" class="img-responsive img-pop pop-1">
+								<img src="image/v02/star.png" class="img-responsive img-pop pop-2">
+								<img src="image/v02/circle.png" class="img-responsive img-pop pop-3">
+
+								<img src="image/v02/thunder.png" class="img-responsive img-pop pop-4">
+								<img src="image/v02/star.png" class="img-responsive img-pop pop-5">
+								<img src="image/v02/circle.png" class="img-responsive img-pop pop-6">
+							</div>
+						</div>
+					</div>
+
+					<div class="row" id="article-body">
+						
+						<!-- body article collection here... -->
+					</div>
+				</div>
+
+				<!-- Fourth content section -->
+				<div class="fourth-content-section">
+					<div class="row">
+						<div class="col-lg-8 col-md-8 col-sm-8 col-8 col-xl-8 mx-auto quiz-horo--collection">
+							<!-- head label collection -->
+							<div class="quiz-head-collection">
+							<p class="quiz-big-head">Quiz<span>ตรงใจ</span></p>
+							<p class="quiz-small-head">จาก a ดวง</p>
+
+							<img src="image/v02/thunder.png" class="img-responsive img-pop pop-1">
+							<img src="image/v02/star.png" class="img-responsive img-pop pop-2">
+							<img src="image/v02/circle.png" class="img-responsive img-pop pop-3">
+
+							<img src="image/v02/thunder.png" class="img-responsive img-pop pop-4">
+							<img src="image/v02/star.png" class="img-responsive img-pop pop-5">
+							<img src="image/v02/circle.png" class="img-responsive img-pop pop-6">
+						</div>
+
+						</div>
+					</div>
+
+					<div class="row" id="quiz-body">
+						
+						<!-- body quiz collection here... -->
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+
+		<!-- footer -->
+		<?php
+		include 'footer.php'
+		?>
+
+	</div>
+
+
+	<!-- jQuery Core Js -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<!-- Popper Core Js -->
+	<script src="vendor/popper/popper.min.js"></script>
+	<!-- Bootstrap Core Js -->
+	<script src="bootstrap-4.0.0-beta-dist/js/bootstrap.min.js"></script>
+	<!-- Tween Max Core Js -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.11.7/TweenMax.min.js"></script>
+
+	<script src="js/create-tarot-deck-2.js"></script>
+
+	<!-- Add loaded class preload -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+
+			setTimeout(function(){
+				$('body').addClass('loaded');
+			}, 2000);
+
+			$(window).scroll(function(){
+				if ($(window).scrollTop() > 950) {
+					$('.navbar-collection').addClass('navbar-fixed-top');
+				} else {
+					$('.navbar-collection').removeClass('navbar-fixed-top');
+				}
+			});
+		});
+	</script>
+
+	<!-- Parallax Script -->
+	<script type="text/javascript">
+		var $layer_1        = $('.mascot-day1'),
+				$layer_2        = $('.mascot-day2'),
+				$layer_3        = $('.mascot-day3'),
+				$layer_4        = $('.mascot-day4'),
+				$layer_5        = $('.mascot-day5'),
+				$layer_6        = $('.mascot-day6'),
+				$layer_7        = $('.mascot-day7'),
+				$layer_8        = $('.mascot-day8'),
+				$collect_center = $('.cover-backpic'),
+				$label_layer    = $('.cover-backpic'),
+				$shadow_layer   = $('.img-mascot'),
+				$pop_trans_1    = $('.pop-trans-1'),
+				$pop_trans_2    = $('.pop-trans-2'),
+				$pop_trans_3    = $('.pop-trans-3'),
+				$container      = $('.wrapper'),
+				container_w     = $container.width(),
+				container_h     = $container.height();
+
+		$(window).on('mousemove.parallax', function(event) {
+			var pos_x = event.pageX,
+					pos_y = event.pageY,
+					left  = 0,
+					top   = 0;
+
+			left = container_w / 2 - pos_x;
+			top  = container_h / 2 - pos_y;
+
+			TweenMax.to(
+				$layer_1,
+				1,
+				{
+					css: {
+						transform: 'translateX(' + left / 12 + 'px) translateY(' + top / 20 + 'px)'
+ 					},
+ 					ease:Expo.easeOut,
+ 					overwirte: 'all'
+				});
+
+			TweenMax.to(
+				$layer_2,
+				1,
+				{
+					css: {
+						transform: 'translateX(' + left / 18 + 'px) translateY(' + top / 22 + 'px)'
+					},
+					ease:Expo.easeOut,
+					overwirte: 'all'
+				});
+
+			TweenMax.to(
+				$layer_3,
+				1,
+				{
+					css: {
+						transform: 'translateX(' + left / 24 + 'px) translateY(' + top / 26 + 'px)'
+					},
+					ease:Expo.easeOut,
+					overwirte: 'all'
+				});
+
+			TweenMax.to(
+				$layer_4,
+				1,
+				{
+					css: {
+						transform: 'translateX(' + left / 16 + 'px) translateY(' + top / 22 + 'px)'
+					},
+					ease:Expo.easeOut,
+					overwirte: 'all'
+				});
+
+			TweenMax.to(
+				$layer_5,
+				1,
+				{
+					css: {
+						transform: 'translateX(' + left / 12 + 'px) translateY(' + top / 20 + 'px)'
+					},
+					ease:Expo.easeOut,
+					overwirte: 'all'
+				});
+
+
+			TweenMax.to(
+				$layer_6,
+				1,
+				{
+					css: {
+						transform: 'translateX(' +  left / 28 + 'px) translateY(' + top / 18 + 'px)'
+					},
+					ease:Expo.easeOut,
+					overwirte: 'all'
+				});
+
+			TweenMax.to(
+				$layer_7,
+				1,
+				{
+					css: {
+						transform: 'translateX(' + left / 30 + 'px) translateY(' + top / 32 + 'px)'
+					},
+					ease:Expo.easeOut,
+					overwirte: 'all'
+				});
+
+			TweenMax.to(
+				$layer_8,
+				1,
+				{
+					css: {
+						transform: 'translateX(' + left / 30 + 'px) translateY(' + top / 32 + 'px)'
+					},
+					ease:Expo.easeOut,
+					overwirte: 'all'
+				});
+
+			TweenMax.to(
+				$collect_center,
+				1,
+				{
+					css: {
+						transform: 'translateX(' + left / 50 + 'px) translateY(' + top / 80 + 'px)'
+					},
+					ease:Expo.easeOut,
+					overwirte: 'all'
+				});
+
+			TweenMax.to(
+				$pop_trans_1,
+				1,
+				{
+					css: {
+						transform: 'translateX(' + left / 80 + 'px) translateY(' + top / 82 + 'px)'
+					},
+					ease:Expo.easeOut,
+					overwirte: 'all'
+				});
+
+			TweenMax.to(
+				$pop_trans_2,
+				1,
+				{
+					css: {
+						transform: 'translateX(' + left / 120 + 'px) translateY(' + top / 150 + 'px)'
+					},
+					ease:Expo.easeOut,
+					overwirte: 'all'
+				});
+
+			TweenMax.to(
+				$pop_trans_3,
+				1,
+				{
+					css: {
+						transform: 'translateX(' + left / 80 + 'px) translateY(' + top / 180 + 'px)'
+					},
+					ease:Expo.easeOut,
+					overwirte: 'all'
+				});
+			
+		});  
+	</script>
+</body>
+</html>
